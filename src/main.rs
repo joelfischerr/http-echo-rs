8cc6b23f78bb906b96a584bf6133554fba2ab14c
Joel Fischer <jfjoel.fischer@gmail.com>
2025-12-13 17:33:41 +0100

    Use example from ModSecurity

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ use clap::Parser;
 use std::{collections::HashMap, str::FromStr, time::Duration};
 use tokio::signal::unix::{signal, SignalKind};
 
+use modsecurity::{ModSecurity, Rules};
+
 #[derive(Parser)]
 struct Args {
     #[arg(short, long, default_value = "0.0.0.0:8000")]
@@ -74,6 +76,36 @@ async fn main() {
     let app = Router::new()
         .route("/", echo.clone())
         .route("/{path}", echo);
+
+    let ms = ModSecurity::default();
+
+    let mut rules = Rules::new();
+    rules
+        .add_plain(
+            r#"
+        SecRuleEngine On
+
+        SecRule REQUEST_URI "@rx admin" "id:1,phase:1,deny,status:401"
+    "#,
+        )
+        .expect("Failed to add rules");
+
+    let mut transaction = ms
+        .transaction_builder()
+        .with_rules(&rules)
+        .build()
+        .expect("Error building transaction");
+
+    transaction
+        .process_uri("http://example.com/admin", "GET", "1.1")
+        .expect("Error processing URI");
+    transaction
+        .process_request_headers()
+        .expect("Error processing request headers");
+
+    let intervention = transaction.intervention().expect("Expected intervention");
+
+    assert_eq!(intervention.status(), 401);
 
     let listener = tokio::net::TcpListener::bind(address.clone())
         .await
